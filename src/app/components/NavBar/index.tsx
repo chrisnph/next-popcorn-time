a19728c1c49d86091be22e930c9fe11c4afd2002
chrisnph <devchrisnph@gmail.com>
2023-12-14 09:57:25 +0800

    feat: components > add NavBar

---
+++ src/app/components/NavBar/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <header className="bg-black text-[#B6FFF5] py-[20px] font-medium text-[1.2rem]">
+      <nav className="nav flex justify-between items-center px-[1rem] md:px-[5rem]">
+        <Link href="/" className="">
+            <img src="/logo.png" className="max-h-[50px] w-auto" />
+        </Link>
+        <div className="space-x-10">
+          <Link href="/movie">Movies</Link>
+          <Link href="/tvShows">TV Shows</Link>
+        </div>
+
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
